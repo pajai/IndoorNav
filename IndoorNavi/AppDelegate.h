06b278287a945782daeafe8ed3dcb37a93a8a381
Patrick Jayet <pja@macbook-pro.extrabright.com>
2013-08-20 22:02:22 +0200

    Initial Commit

---
+++ IndoorNavi/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  IndoorNavi
+//
+//  Created by Patrick Jayet on 20/08/13.
+//  Copyright (c) 2013 Patrick Jayet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
